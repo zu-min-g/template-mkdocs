168b167f0df904acac4eb28ff946cad7205cae9a
zu-min <68736447+zu-min-g@users.noreply.github.com>
2021-06-14 20:21:57 +0900

    色の切り替えに対応

--- docs/assets/javascripts/extra.js
+++ docs/assets/javascripts/extra.js
@@ -1,20 +1,34 @@
-mermaid.initialize({
-  'theme': 'dark',
-  'themeVariables': {
-  },
-});
-
-window.addEventListener('load', function () {
-  var svgs = d3.selectAll(".mermaid svg");
-  svgs.each(function() {
-    var svg = d3.select(this);
-    svg.html("<g>" + svg.html() + "</g>");
-    var inner = svg.select("g");
-    var zoom = d3.zoom().filter(function (event) {
-      return event.shiftKey;
-    }).on("zoom", function(event) {
-      inner.attr("transform", event.transform);
+(function () {
+  function initializeMermaid() {
+    var schema = document.body.getAttribute("data-md-color-scheme");
+    var isDarkMode = schema == "slate";
+    mermaid.initialize({
+      'theme': isDarkMode ? 'dark' : 'base',
+      'themeVariables': {
+      },
     });
-    svg.call(zoom);
+  }
+
+  window.addEventListener('load', function () {
+    initializeMermaid();
+    var svgs = d3.selectAll(".mermaid svg");
+    svgs.each(function() {
+      var svg = d3.select(this);
+      svg.html("<g>" + svg.html() + "</g>");
+      var inner = svg.select("g");
+      var zoom = d3.zoom().filter(function (event) {
+        return event.shiftKey;
+      }).on("zoom", function(event) {
+        inner.attr("transform", event.transform);
+      });
+      svg.call(zoom);
+    });
+
+    var options = document.getElementsByClassName("md-option");
+    for (var elem of options) {
+      elem.addEventListener('change', () => {
+        initializeMermaid();
+      });
+    }
   });
-});
+})();
